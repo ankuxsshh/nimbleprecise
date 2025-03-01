7759d44e6d103be2000a6f0ee3e863429f85442c
ankuxsshh <ankuxshh72@gmail.com>
2025-03-01 16:56:14 +0530

    first commit

--- nimble_app/urls.py
+++ nimble_app/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('team', views.team, name='team'),
     path('blogs', views.blogs, name='blogs'),
+    path('abacus', views.abacus, name='abacus'),
+    path('rubikscube', views.rubikscube, name='rubikscube'),
+    path('vedicmaths', views.vedicmaths, name='vedicmaths'),
+    path('techworld', views.techworld, name='techworld'),
 ]
--- nimble_app/views.py
+++ nimble_app/views.py
@@ -19,3 +19,15 @@ def contact(request):
 
 def blogs(request):
     return render(request, 'blogs.html')
+
+def abacus(request):
+    return render(request, 'abacus.html')
+
+def vedicmaths(request):
+    return render(request, 'vedicmaths.html')
+
+def techworld(request):
+    return render(request, 'techworld.html')
+
+def rubikscube(request):
+    return render(request, 'rubikscube.html')
